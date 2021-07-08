12217d6ced0b10cd1e4853a569bf55c6ab2a8509
sariwahyu <32873347+sariwahyu@users.noreply.github.com>
2021-07-09 01:13:56 +0700

    supermarket list error solved

--- lib/model/supermarket_list.dart
+++ lib/model/supermarket_list.dart
@@ -1,18 +1,14 @@
-import 'dart:ffi';
-import 'dart:html';
-
-import 'package:flutter/material.dart';
-import 'package:flutter_atms/supermarket.dart';
-
 class SupermarketItems {
   String itemName;
+  double itemPrice;
   String supermarketLocation;
   double supermarketDistance;
-  List<Map<String,TimeElement>> openDays;
+  List<Map<String, double>> openDays;
   String supermarketDescription;
 
   SupermarketItems({
     required this.itemName,
+    required this.itemPrice,
     required this.supermarketLocation,
     required this.supermarketDistance,
     required this.openDays,
@@ -22,17 +18,48 @@ class SupermarketItems {
 
 var supermarketItems = [
   SupermarketItems(
-    itemName: 'Pepaya', 
-    supermarketLocation: 'Giant Keputih, Sukolilo', 
-    supermarketDistance: 100.99, 
-    openDays: [
-      'Senin' : 08.00-21.00,
-      'Selasa' : 08.00-21.00,
-      'Rabu' : 08.00-21.00,
-      'Kamis' : 08.00-21.00,
-      'Jumat' : 08.00-21.00,
-      'Sabtu' : 08.00-21.00,
-      'Minggu' : 08.00-21.00,
-      ], 
-    supermarketDescription: 'supermarketDescription'),
+      itemName: 'Apel Fuji',
+      itemPrice: 16000,
+      supermarketLocation: 'Giant Keputih, Sukolilo',
+      supermarketDistance: 100.99,
+      openDays: [
+        {'Senin': 08.00 - 21.00},
+        {'Selasa': 08.00 - 21.00},
+        {'Rabu': 08.00 - 21.00},
+        {'Kamis': 08.00 - 21.00},
+        {'Jumat': 08.00 - 21.00},
+        {'Sabtu': 08.00 - 21.00},
+        {'Minggu': 08.00 - 21.00},
+      ],
+      supermarketDescription: 'supermarketDescription'),
+  SupermarketItems(
+      itemName: 'Keju',
+      itemPrice: 20000,
+      supermarketLocation: 'Superindo Keputih, Sukolilo',
+      supermarketDistance: 50.99,
+      openDays: [
+        {'Senin': 08.00 - 21.00},
+        {'Selasa': 08.00 - 21.00},
+        {'Rabu': 08.00 - 21.00},
+        {'Kamis': 08.00 - 21.00},
+        {'Jumat': 08.00 - 21.00},
+        {'Sabtu': 08.00 - 21.00},
+        {'Minggu': 08.00 - 21.00},
+      ],
+      supermarketDescription: 'supermarketDescription'),
+  SupermarketItems(
+      itemName: 'Pepaya',
+      itemPrice: 8000,
+      supermarketLocation: 'Pasar Keputih, Sukolilo',
+      supermarketDistance: 100.99,
+      openDays: [
+        {'Senin': 08.00 - 22.00},
+        {'Selasa': 08.00 - 22.00},
+        {'Rabu': 08.00 - 22.00},
+        {'Kamis': 08.00 - 22.00},
+        {'Jumat': 08.00 - 22.00},
+        {'Sabtu': 08.00 - 22.00},
+        {'Minggu': 08.00 - 00.00},
+      ],
+      supermarketDescription: 'supermarketDescription'),
 ];
